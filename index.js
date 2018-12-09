10219c4ec4a34c281b19469bf04298ced3323f5d
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-09 15:00:49 +0530

    unique URL generator added

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var router = express();
+const app = require('express')();
+const path = require('path');
+const multer = require('multer');
+const shortid = require('shortid');
 
-var multer = require("multer");
-var upload = multer({ dest: 'uploads' })
-
-router.post('/restapi', upload.single('file'), function (req, res, next) {
-    if (!req.file) {
-        res.json({ "error": "not working" });
-        return;
+var storage = multer.diskStorage({
+    destination: 'uploads',
+    filename: function(req, file, cb){
+        const id = shortid.generate();
+        file.url = id;
+        cb(null, id + path.extname(file.originalname))
     }
-    console.log(req.file);
-    const filename = req.file.filename.substring(0, 5);
-    console.log("Saving as..", filename);
-    
-    res.json({ "url": filename });
-    return;
+})
+
+const upload = multer({
+    storage
+}).single('file');
+
+app.post('/upload', (req, res)=>{
+    upload(req, res, function(err){
+        if(err){
+            res.json({key: 'Something went wrong', err });
+        }
+        const url = req.file.url;
+        console.log("Req", url);
+        res.json({url:'http://i.sprk.pw/'+ url});
+    });
 });
 
-router.listen(3000, () => console.log('http://localhost:3000/'))
+
+app.listen(3000, () => console.log('http://localhost:3000/'));
