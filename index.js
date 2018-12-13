e34d6d3d9aa5bb9b2c69c43ea36fdba79bfb4732
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-13 22:07:12 +0530

    went full on fp, still needs improvement

--- index.js
+++ index.js
@@ -17,34 +17,51 @@ const MAX_AGE = 30; // DAYS
 const MAX_SIZE = 2000; // 2MB
 const DOMAIN = 'http://localhost:3000/'; // Mind the training slash (/)
 
+function getRetentionPeriod (stat) {
+    return MIN_AGE + (-MAX_AGE + MIN_AGE) * Math.pow((parseInt(stat.size / 1000.0) / MAX_SIZE - 1), 3);
+}
+
+function uploadsPath (childPath = '') {
+    return path.resolve(__dirname, 'uploads', childPath);
+}
+
+function getFileStats (file) {
+    const stat = fs.statSync(path.join(uploadsPath(), file));
+    const retention = getRetentionPeriod(stat);
+    return {
+        file,
+        size: parseInt(stat.size / 1000.0),
+        date: stat.mtime,
+        retention: parseInt(retention),
+    };
+}
 /**
  * Cron job runs every At 04:05 on Sunday. (random, yes)
  */
 const job = new CronJob('5 4 * * sun', () => {
-    const uploadsDir = path.resolve(__dirname, 'uploads');
-    let filesMetadata = [];
-    const files = fs.readdirSync(uploadsDir);
-    files.forEach(function (file, index) {
-        const stat = fs.statSync(path.join(uploadsDir, file));
-        const retention = MIN_AGE + (-MAX_AGE + MIN_AGE) * Math.pow((parseInt(stat.size / 1000.0) / MAX_SIZE - 1), 3);
-        filesMetadata.push({
-            file,
-            size: parseInt(stat.size / 1000.0),
-            date: stat.mtime,
-            retention: parseInt(retention),
-        }); // convert size to KB. use 1000000 for MB.
-    });
-    filesMetadata.forEach((file) => {
-        const diff = differenceInDays(new Date(), file.date);
-        if (diff > file.retention) {
-            fs.unlink(path.resolve(uploadsDir, file.file), (err) => {
-                if (err) {
-                    console.error('Error while deleting', err);
-                }
-                console.log('Successsfully deleted the file', file.file);
-            });
-        }
-    });
+    // ####### Blocking #######
+    fs.readdirSync(uploadsPath())
+        .map(file => {
+            const stat = fs.statSync(path.join(uploadsPath(), file));
+            const retention = getRetentionPeriod(stat);
+            return {
+                file,
+                size: parseInt(stat.size / 1000.0),
+                date: stat.mtime,
+                retention: parseInt(retention),
+            };
+        })
+        .map(file => {
+            const diff = differenceInDays(new Date(), file.date);
+            if (diff > file.retention) {
+                fs.unlink(uploadsPath(file.file), (err) => {
+                    if (err) {
+                        console.error('Error while deleting', err);
+                    }
+                    console.log('Successsfully deleted the file', file.file);
+                });
+            }
+        });
 }, null, false, 'Asia/Kolkata'); // Also exposes .start() chained method
 
 job.start();
@@ -70,7 +87,7 @@ app.post('/', (req, res) => {
     }
 });
 
-app.get('/favicon.ico', (req, res) => res.sendFile(path.resolve(__dirname, 'favicon.ico')));
+app.get('/favicon.ico', (req, res) => res.sendFile(uploadsPath('favicon.ico')));
 
 app.get('/:file', (req, res, next) => {
     const API_KEY_HEADER = req.get('x-api-key');
@@ -82,12 +99,12 @@ app.get('/:file', (req, res, next) => {
         res.end();
     } else {
         const requestedFile = req.params.file;
-        fs.readFile(path.resolve(__dirname, 'uploads', requestedFile), readErr => {
+        fs.readFile(uploadsPath(requestedFile), readErr => {
             if (readErr) {
                 console.error('File reading Error', readErr);
                 res.end('File not found');
             }
-            res.sendFile(path.resolve(__dirname, 'uploads', requestedFile), err => {
+            res.sendFile(uploadsPath(requestedFile), err => {
                 if (err) {
                     console.error('Error while sending the file', err);
                     res.end('Cannot send the specified file');
