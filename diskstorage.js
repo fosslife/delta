0fb63a662ad0f654fc94bbd7dbc3d28a00a43be9
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-15 14:24:26 +0530

    added alternative way to generate unique strings

--- diskstorage.js
+++ diskstorage.js
@@ -7,6 +7,8 @@ const path = require('path');
 const storage = multer.diskStorage({
     destination: 'uploads',
     filename: (req, file, cb) => {
+        // Use Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, lenthYouWant);
+        // If you don't want shortId, 'pretty much' random + alphabets only
         const id = shortid.generate();
         file.url = id + path.extname(file.originalname);
         cb(null, id + path.extname(file.originalname));
