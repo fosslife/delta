4e5baf375d975957139f272dba2ebe4b20681e26
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-23 12:03:19 +0530

    fixed uploads path dirname for systemd service

--- core/diskstorage.js
+++ core/diskstorage.js
@@ -5,7 +5,7 @@ const shortid = require('shortid');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: 'uploads',
+    destination: path.resolve(__dirname, '../uploads'),
     filename: (req, file, cb) => {
         // Use Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, lenthYouWant);
         // If you don't want shortId, 'pretty much' random + alphabets only
