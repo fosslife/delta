edbb50177a4cd479883b8d7fed5e7059d5c7118d
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-25 15:50:28 +0530

    refactored file uploader logic to a separate file to support URLs too

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const path = require('path');
 const fs = require('fs');
 const job = require('./core/cron');
 const { env, domainUrl } = require('./config');
-const isAuthorizedUser = require('./core/isAuthorizedUser');
 const { promisify } = require('util');
-const upload = promisify(require('./core/diskstorage').upload);
 const db = require('./core/db');
 const logger = require('./core/logger');
 const expressip = require('express-ip');
+const fileUploader = require('./core/fileUploader');
 
 db.defaults({ files: [] }).write();
 app.use(expressip().getIpInfoMiddleware);
@@ -39,28 +38,11 @@ if (env === 'PROD') {
  * POST method to upload file
  */
 app.post('/', (req, res) => {
-    const API_KEY_HEADER = req.get('api-key');
-    const responseStatus = isAuthorizedUser(API_KEY_HEADER);
-    if (responseStatus === 200) {
-        upload(req, res)
-            .then(() => {
-                const originalName = req.file.originalname;
-                const shortened = req.file.filename;
-                logger.info('Uploading ' + JSON.stringify(originalName) + ' as ' + shortened);
-                db
-                    .get('files')
-                    .push({ 'originalName': originalName, 'shortened': shortened })
-                    .write();
-                const url = `${DOMAIN}${req.file.url}\n`;
-                res.end(url);
-            })
-            .catch(err => {
-                logger.error('Error while uploading the file ' + err);
-                res.end('Something went wrong while uploading the file');
-            });
+    const requestBody = req.body;
+    if (!requestBody.url) {
+        fileUploader(req, res);
     } else {
-        logger.error('Unauthorized user visit ' + JSON.stringify(req.ipInfo));
-        responseStatus === 401 ? res.status(responseStatus).send('Unauthorized \n') : res.status(responseStatus).send('Forbidden \n');
+        res.end('shortened url: abcd \n');
     }
 });
 
