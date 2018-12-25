820bedfad3f9c4d404cdf96a6843c8a4dee5be40
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-25 18:08:05 +0530

    shortened URLs are being generated

--- core/urlShortner.js
+++ core/urlShortner.js
@@ -1,10 +1,17 @@
 const { encode } = require('./shortURL');
 const db = require('./db');
+const { env, domainUrl } = require('../config');
+const DOMAIN = env === 'PROD' ? domainUrl : 'http://localhost:3000/';
 
 const urlShortener = (req, res) => {
     const URL = req.body.url;
-    db.get('files').inc().value();
-    res.end(encode(URL.length));
+    const uid = db.get('uniqueID').value();
+    const shortURL = encode(uid);
+    const id = db.get('urls').size() + 1;
+    db.get('urls').push({ id, originalURL: URL, shortenedURL: shortURL }).write();
+    db.set('uniqueID', uid + 1).write();
+    res.write(`${DOMAIN}${shortURL}`);
+    res.end('\n');
 };
 
 module.exports = urlShortener;
