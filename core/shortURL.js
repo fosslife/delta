8dd8e3439e73e8568dcea77eee186bea8801e33b
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-25 16:15:49 +0530

    Added shortURL

---
+++ core/shortURL.js
@@ -0,0 +1,41 @@
+/*
+ * ShortURL (https://github.com/delight-im/ShortURL)
+ * Copyright (c) delight.im (https://www.delight.im/)
+ * Licensed under the MIT License (https://opensource.org/licenses/MIT)
+ */
+
+/**
+ * ShortURL: Bijective conversion between natural numbers (IDs) and short strings
+ *
+ * ShortURL.encode() takes an ID and turns it into a short string
+ * ShortURL.decode() takes a short string and turns it into an ID
+ *
+ * Features:
+ * + large alphabet (51 chars) and thus very short resulting strings
+ * + proof against offensive words (removed 'a', 'e', 'i', 'o' and 'u')
+ * + unambiguous (removed 'I', 'l', '1', 'O' and '0')
+ *
+ * Example output:
+ * 123456789 <=> pgK8p
+ */
+
+const _alphabet = '23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_';
+const _base = _alphabet.length;
+
+module.exports = {
+    encode: function (num) {
+        var str = '';
+        while (num > 0) {
+            str = _alphabet.charAt(num % _base) + str;
+            num = Math.floor(num / _base);
+        }
+        return str;
+    },
+    decode: function (str) {
+        var num = 0;
+        for (var i = 0; i < str.length; i++) {
+            num = num * _base + _alphabet.indexOf(str.charAt(i));
+        }
+        return num;
+    }
+};
