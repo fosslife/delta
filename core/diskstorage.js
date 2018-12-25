de2457090f2ba086e35c130c34328c020ec370ce
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-25 17:45:45 +0530

    removed shortid url generation

--- core/diskstorage.js
+++ core/diskstorage.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const multer = require('multer');
-const shortid = require('shortid');
 const path = require('path');
 const { encode } = require('./shortURL');
 const db = require('./db');
@@ -9,9 +8,6 @@ const db = require('./db');
 const storage = multer.diskStorage({
     destination: path.resolve(__dirname, '../uploads'),
     filename: (req, file, cb) => {
-        // Use Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, lenthYouWant);
-        // If you don't want shortId, 'pretty much' random + alphabets only
-        // const id = shortid.generate();
         const uid = db.get('uniqueID').value();
         const id = encode(uid);
         console.log('generating id', uid);
