7e177e9b0864370cc77b46d5d5cd0bd0f92d99e7
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-25 15:50:28 +0530

    refactored file uploader logic to a separate file to support URLs too

---
+++ core/fileUploader.js
@@ -0,0 +1,36 @@
+const { promisify } = require('util');
+const upload = promisify(require('./diskstorage').upload);
+const isAuthorizedUser = require('./isAuthorizedUser');
+const logger = require('./logger');
+const db = require('./db');
+const { env, domainUrl } = require('../config');
+
+const DOMAIN = env === 'PROD' ? domainUrl : 'http://localhost:3000/';
+
+const fileUploader = (req, res) => {
+    const API_KEY_HEADER = req.get('api-key');
+    const responseStatus = isAuthorizedUser(API_KEY_HEADER);
+    if (responseStatus === 200) {
+        upload(req, res)
+            .then(() => {
+                const originalName = req.file.originalname;
+                const shortened = req.file.filename;
+                logger.info('Uploading ' + JSON.stringify(originalName) + ' as ' + shortened);
+                db
+                    .get('files')
+                    .push({ 'originalName': originalName, 'shortened': shortened })
+                    .write();
+                const url = `${DOMAIN}${req.file.url}\n`;
+                res.end(url);
+            })
+            .catch(err => {
+                logger.error('Error while uploading the file ' + err);
+                res.end('Something went wrong while uploading the file \n');
+            });
+    } else {
+        logger.error('Unauthorized user visit ' + JSON.stringify(req.ipInfo));
+        responseStatus === 401 ? res.status(responseStatus).send('Unauthorized \n') : res.status(responseStatus).send('Forbidden \n');
+    }
+};
+
+module.exports = fileUploader;
