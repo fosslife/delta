166e8aeaf1844b9159a8137d6cfa5c17a0b92e09
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-26 13:07:41 +0530

    fixed creating storage directory if not present

--- core/db.js
+++ core/db.js
@@ -1,7 +1,14 @@
 const lowdb = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-const path = require('path');
-const adapter = new FileSync(path.resolve(__dirname, '../storage', 'db.json'));
+const { resolve } = require('path');
+const { existsSync, mkdirSync } = require('fs');
+const storageExists = existsSync(resolve(__dirname, '..', 'storage'));
+
+if (!storageExists) {
+    mkdirSync(resolve(__dirname, '..', 'storage'));
+}
+
+const adapter = new FileSync(resolve(__dirname, '..', 'storage', 'db.json'));
 const db = lowdb(adapter);
 
 module.exports = db;
