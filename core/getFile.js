bf5995634409a7665a7f014e7e0c9a28552e0ee6
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-26 14:25:19 +0530

    Removed unnecessary file reading logic

--- core/getFile.js
+++ core/getFile.js
@@ -1,5 +1,3 @@
-const { promisify } = require('util');
-const readFileAsync = promisify(require('fs').readFile);
 const { resolve } = require('path');
 const logger = require('./logger');
 
@@ -8,20 +6,13 @@ const uploadsPath = (childPath = '') => {
 };
 
 const getFile = (filename, req, res) => {
-    readFileAsync(uploadsPath(filename))
-        .then((f) => {
-            res.sendFile(uploadsPath(filename))
-                .then(() => {
-                    logger.info('File sent ' + filename);
-                })
-                .catch(fileUploadError => {
-                    logger.error('Error while sending the file ' + fileUploadError);
-                    res.end('Error while sending file, please contact admin');
-                });
+    res.sendFile(uploadsPath(filename))
+        .then(() => {
+            logger.info('File sent ' + filename);
         })
-        .catch(readErr => {
-            logger.error('File reading Error ' + readErr);
-            res.end('File not found');
+        .catch(fileUploadError => {
+            logger.error('Error while sending the file ' + fileUploadError);
+            res.end('Error while sending file, please contact admin');
         });
 };
 
