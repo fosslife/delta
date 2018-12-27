04f3e891911fd2d9ec37dbc97f9559f024646bff
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-27 15:22:11 +0530

    destination directory creation login added

--- core/files/diskstorage.js
+++ core/files/diskstorage.js
@@ -5,8 +5,21 @@ const path = require('path');
 const reqLib = require('app-root-path').require;
 const { encode } = reqLib('core/urls/shortURL');
 const db = reqLib('core/db');
+const { existsSync, mkdirSync } = require('fs');
+
 const storage = multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    // path.resolve(__dirname, '..', '..', 'uploads')
+    destination: (req, file, cb) => {
+        const uniquePath = path.resolve(__dirname, '..', '..', 'uploads', req.get('api-key'));
+        const rootPath = path.resolve(__dirname, '..', '..', 'uploads');
+        if (!existsSync(rootPath)) {
+            mkdirSync(rootPath);
+        }
+        if (!existsSync(uniquePath)) {
+            mkdirSync(uniquePath);
+        }
+        cb(null, uniquePath);
+    },
     filename: (req, file, cb) => {
         const uid = db.get('uniqueID').value();
         const id = encode(uid);
