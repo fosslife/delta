cc15330d19f9b37d607bcf0b20811c9cb76d0946
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2019-10-02 14:16:10 +0530

    [add] prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 80,
+    endOfLine: 'lf',
+    arrowParens: 'avoid',
+    trailingComma: 'none',
+    semi: true,
+    useTabs: false,
+    singleQuote: true,
+    bracketSpacing: true,
+    jsxBracketSameLine: false
+};
