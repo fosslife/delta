ecda9e4bf706cf9d393b5548d8cf9dbc041d48e4
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-27 13:59:05 +0530

    changed config to js

---
+++ config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    env: 'DEVELOPMENT',
+    timeZone: 'Asia/Kolkata',
+    users: [
+        ['spark', 's', 'https://i.sprk.pw/'],
+        ['pwnj', 'p', 'https://pwnj.pw/']
+    ],
+    cron: '10 5 * 1-12/2'
+};
