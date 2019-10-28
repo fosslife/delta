4277b343066967529cf2229755f75cd9bf699dae
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-28 16:57:03 +0530

    finalized never clashing random URLs

--- core/db.js
+++ core/db.js
@@ -1,12 +1,42 @@
 'use strict';
 const Redis = require('ioredis');
 const db = new Redis();
+const manager = new Redis();
+const nanoid = require('nanoid/generate');
 
-if (process.env.NODE_ENV === 'development') {
+manager.subscribe('removed');
+
+manager.on('message', async function(channel, message) {
+    const count = await db.scard('genurls');
+    if (count < 5) {
+        console.log('db has', count, 'urls, generating more urls');
+        generateUrls(5);
+    }
+    console.log('Got', message, 'from channel', channel);
+});
+
+db.on('connect', function() {
+    console.log('Connected to redis instance');
+    generateUrls(10);
+});
+
+if (process.env.NODE_ENV === 'developments') {
     (async () => {
         const monitor = await db.monitor();
         monitor.on('monitor', console.log);
     })();
 }
-
+function generateUrls(n) {
+    let length = 4;
+    for (let i = 0; i < n; i++) {
+        if (i === 5000) {
+            length += 1;
+        }
+        const id = nanoid(
+            '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-',
+            length
+        );
+        db.sadd('genurls', id);
+    }
+}
 module.exports = db;
--- core/urls/urlShortner.js
+++ core/urls/urlShortner.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const { encode } = require('../urls/shortURL');
 const db = require('../db');
 const validURL = require('valid-url');
 const isAuthorizedUser = require('../isAuthorizedUser');
@@ -19,41 +18,21 @@ const urlShortener = async (req, res) => {
         const isURL = validURL.isWebUri(URL);
         if (isURL) {
             logger.info(`User has given valid URL`);
-            const uid = await db.get('index');
             // part of URL either custom or incremented-auto-url
-            const customOrAuto = specialURL || encode(uid);
+            const customOrAuto = specialURL || (await db.spop('genurls'));
+            await db.publish('removed', 'remove');
             const fullURL = specialURL
                 ? `${domain}${customOrAuto}`
                 : `${domain}${customOrAuto}`;
-            const prevExists = await db.sismember('urls', customOrAuto);
-            if (prevExists) {
-                if (specialURL) {
-                    logger.info(`Previous Custom URL exists, aborting`);
-                    res.end(
-                        `URL with ID ${customOrAuto} already exists. Try another custom path\n`
-                    );
-                } else {
-                    logger.info(`URLs clashed, impossible tho!`);
-                    await db.incr('index');
-                    // Can give specific message but don't want user to know that URL with
-                    // same ID already exists as it's random not custom. So generic error.
-                    res.end(
-                        `Error occurred, try again. If error persists, file an Issue`
-                    );
-                }
-            } else {
-                logger.info(`Got shortUrl ${customOrAuto}. updating DB`);
-                await db.hset(
-                    `short:${customOrAuto}`,
-                    'original',
-                    URL,
-                    'type',
-                    'url'
-                );
-                await db.incr('index');
-                await db.sadd('urls', customOrAuto);
-                res.end(`${fullURL}\n`);
-            }
+            logger.info(`Got shortUrl ${customOrAuto}. updating DB`);
+            await db.hset(
+                `short:${customOrAuto}`,
+                'original',
+                URL,
+                'type',
+                'url'
+            );
+            res.end(`${fullURL}\n`);
         } else {
             logger.error('User gave invalid URL');
             res.end('Please enter a valid resource URL\n');
