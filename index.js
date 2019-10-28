97e4b93a04cb5b997dc55c4e4ae816aa1d0c31dc
Sparkenstein <zetabytes.pp@gmail.com>
2019-10-28 12:30:38 +0530

    welcome note on index route

--- index.js
+++ index.js
@@ -11,49 +11,54 @@ const logger = require('./core/logger');
 const { resolve } = require('path');
 const { NODE_ENV: env } = process.env;
 const uploads = require('./routes/router');
-/**
- * Middlewares and inits
- */
-// Initialize index by 1000
-db.get('index').then(id => {
-    if (!id) {
-        db.incrby('index', 1000);
+
+async function main() {
+    /**
+     * Middlewares and inits
+     */
+    // Initialize index by 1000
+    db.get('index').then(id => {
+        if (!id) {
+            db.incrby('index', 1000);
+        }
+    });
+
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(helmet());
+
+    express.response.sendFile = promisify(express.response.sendFile);
+
+    if (env === 'production') {
+        const job = require('./core/cron');
+        job.start();
+        logger.info('Cronjob starting in production mode');
     }
-});
+    /**
+     * Router
+     */
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(helmet());
+    /**
+     * Note that favicon is kinda important here
+     * as all the routes are analogues /, /favicon, /:url are
+     * same for express, so if /favicon fails, it will try to
+     * search for /favicon in URLs instead :(
+     */
+    app.get('/favicon.ico', (req, res) =>
+        res.sendFile(resolve(__dirname, './favicon.png'))
+    );
 
-express.response.sendFile = promisify(express.response.sendFile);
+    app.use('/', uploads);
 
-if (env === 'production') {
-    const job = require('./core/cron');
-    job.start();
-    logger.info('Cronjob starting in production mode');
+    app.get('/', (req, res) => {
+        // res.writeHead(200, { 'Content-Type': 'text/plain charset=us-ascii' });
+        res.sendFile(resolve(__dirname, 'welcome.txt'));
+    });
+
+    app.listen(3000, () =>
+        // eslint-disable-next-line
+        console.log('Server started at port 3000')
+    );
 }
-/**
- * Router
- */
 
-/**
- * Note that favicon is kinda important here
- * as all the routes are analogues /, /favicon, /:url are
- * same for express, so if /favicon fails, it will try to
- * search for /favicon in URLs instead :(
- */
-app.get('/favicon.ico', (req, res) =>
-    res.sendFile(resolve(__dirname, './favicon.png'))
-);
-
-app.use('/', uploads);
-
-app.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain charset=us-ascii' });
-    res.end('Hello\n');
-});
-
-app.listen(3000, () =>
-    // eslint-disable-next-line
-    console.log('Server started at port 3000')
-);
+main();
