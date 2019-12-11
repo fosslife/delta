22f8c01320a43aec14d1a70e5ef2dfc5fdbdbd3c
Sparkenstein <zetabytes.pp@gmail.com>
2019-12-11 13:55:23 +0530

    [fail] - script doesn't work, need help

--- up.sh
+++ up.sh
@@ -12,14 +12,20 @@ Args-
   password
 
 Example:
-To upload file
-    ./up.sh file filename.txt http://spark.pepe/
-To upload a file and password protect it
-    ./up.sh file filename.txt password1234 http://spark.pepe/
-To shorten a URL
-    ./up.sh url http://www.longurl.domain/path http://spark.pepe/
-To shorten a URL and password protect it
-    ./up.sh url http://www.longurl.domain/path password1234 http://spark.pepe/
+    To upload file
+        ./up.sh file filename.txt http://spark.pepe/
+    To upload a file and password protect it
+        ./up.sh file filename.txt password1234 http://spark.pepe/
+    To shorten a URL
+        ./up.sh url http://www.longurl.domain/path http://spark.pepe/
+    To shorten a URL and password protect it
+        ./up.sh url http://www.longurl.domain/path password1234 http://spark.pepe/
+    To shorten with custom URL
+        ./up.sh url http://www.longurl.domain/path shortcustom http://spark.pepe/
+    To shorten with custom URL with password
+        ./up.sh url http://www.longurl.domain/path shortcustom password123 http://spark.pepe/
+
+i.e. password will always be second last parameter, last is always server url
 EndOfMessage
 }
 
@@ -38,17 +44,7 @@ uploadFile() {
 }
 
 uploadUrl(){
-    case $# in
-        3 )
-            echo "Shortening URL"
-            ;;
-        4 )
-            echo "shortening url with password"
-            ;;
-        * )
-            echo "Incorrect arguments, use --help"
-            exit 1
-    esac
+   # what?
 }
 case $1 in
     file )   
