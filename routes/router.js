141851db7e75b13cfb2e053db9e4273fc6fcb082
Sparkenstein <zetabytes.pp@gmail.com>
2019-12-12 14:37:14 +0530

    [feature] - implemented inmemory cache

--- routes/router.js
+++ routes/router.js
@@ -5,8 +5,21 @@ const db = require('../core/db');
 const logger = require('../core/logger');
 const fileUploader = require('../core/files/fileUploader');
 const urlShortner = require('../core/urls/urlShortner');
-const getFile = require('../core/files/getFile');
 const auth = require('basic-auth');
+
+const cache = new Proxy(
+    {},
+    {
+        set: (target, property, value) => {
+            const totalKeys = Object.keys(target);
+            if (totalKeys.length >= 50) {
+                delete target[totalKeys[0]];
+            }
+            target[property] = value;
+            return true;
+        }
+    }
+);
 
 uploads.post('/', (req, res) => {
     const requestBody = req.body;
@@ -21,41 +34,48 @@ uploads.post('/', (req, res) => {
 
 uploads.get('/:link', async (req, res) => {
     const request = req.params.link;
-    logger.info('Serving file ' + request);
-    const record = await db.hgetall(`short:${request}`);
-    logger.info('Found record' + JSON.stringify(record));
-
-    // TODO: remove duplicate else parts
-    if (record.password) {
-        const user = auth(req);
-        if (!user || user.pass !== record.password) {
-            res.set(
-                'WWW-Authenticate',
-                'Basic realm="url is protected by password"'
-            );
-            return res.status(401).send();
-        } else {
-            if (record.type === 'file') {
-                const fileName = record.path;
-                getFile(fileName, req, res);
-            } else if (record.type === 'url') {
-                res.redirect(record.original);
-            } else {
-                res.end(
-                    'Incorrect link or record is expired and cleaned by cron'
-                );
-            }
-        }
+    logger.info(`Serving file short:${request}`);
+    if (cache[`short:${request}`]) {
+        serve(cache[`short:${request}`], res);
     } else {
-        if (record.type === 'file') {
-            const fileName = record.path;
-            getFile(fileName, req, res);
-        } else if (record.type === 'url') {
-            res.redirect(record.original);
+        const record = await db.hgetall(`short:${request}`);
+        // eslint thinks that it's a race condition. Smart.
+        // but it's not. I update cache only when cache-miss happens.
+        // eslint-disable-next-line
+        cache[`short:${request}`] = record;
+        if (record.password) {
+            const user = auth(req);
+            if (!user || user.pass !== record.password) {
+                res.set(
+                    'WWW-Authenticate',
+                    'Basic realm="url is protected by password"'
+                );
+                return res.status(401).send();
+            } else {
+                serve(record, res);
+            }
         } else {
-            res.end('Incorrect link or record is expired and cleaned by cron');
+            serve(record, res);
         }
     }
 });
+
+function serve(record, res) {
+    if (record.type === 'file') {
+        const fileName = record.path;
+        res.sendFile(fileName)
+            .then(() => {
+                logger.info('File sent ' + record.path);
+            })
+            .catch(fileUploadError => {
+                logger.error('Error while sending the file ' + fileUploadError);
+                res.end('Error while sending file, please contact admin');
+            });
+    } else if (record.type === 'url') {
+        res.redirect(record.original);
+    } else {
+        res.end('Incorrect link or record is expired and cleaned by cron');
+    }
+}
 
 module.exports = uploads;
