f95d4e5855cfbbe61b67587b053a6c8feb748c7d
Sparkenstein <zetabytes.pp@gmail.com>
2019-12-25 21:44:45 +0530

    [feature] - ability to decide type of response

--- core/files/fileUploader.js
+++ core/files/fileUploader.js
@@ -10,6 +10,7 @@ const fileUploader = (req, res) => {
     const API_KEY_HEADER = req.get('api-key');
     const responseStatus = isAuthorizedUser(API_KEY_HEADER);
     if (responseStatus === 200) {
+        const contentType = req.get('Accept') || 'text/plain';
         upload(req, res)
             .then(async () => {
                 const shortened = req.file.url;
@@ -34,12 +35,21 @@ const fileUploader = (req, res) => {
                     await db.expire(`short:${shortened}`, duration);
                     await db.hset(`short:${shortened}`, 'expires', duration);
                 }
-                const url = `${req.file.domain}${req.file.url}\n`;
-                res.end(url);
+                const url = `${req.file.domain}${req.file.url}`;
+                if (contentType === 'application/json') {
+                    res.json({
+                        message: url,
+                        status: 200
+                    });
+                } else {
+                    res.end(`${url}\n`);
+                }
             })
             .catch(err => {
                 logger.error('Error while uploading the file ' + err);
-                res.end('Something went wrong while uploading the file \n');
+                res.status(500).json({
+                    message: 'Something went wrong while uploading the file'
+                });
             });
     } else {
         logger.error('Unauthorized user visit ' + JSON.stringify(req.ip));
