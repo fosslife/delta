e13fd697760ab0ae75f1c9b3b5ae8d8f63e83ba2
Sparkenstein <zetabytes.pp@gmail.com>
2020-04-21 20:08:19 +0530

    [ui] - Fixed few bugs, UI made better

--- public/main.js
+++ public/main.js
@@ -5,6 +5,7 @@ const fileButton = document.getElementById('fileButton');
 const urlInput = document.getElementById('url');
 const fileInput = document.getElementById('file');
 const resultDiv = document.getElementById('result');
+const loader = document.getElementById('loader');
 
 URLButton.addEventListener('click', async () => {
     const url = urlInput.value;
@@ -12,6 +13,10 @@ URLButton.addEventListener('click', async () => {
         alert('No URL specified');
         return;
     }
+    if (resultDiv.innerHTML) {
+        resultDiv.innerHTML = '';
+    }
+    loader.classList.remove('invisible');
     const headers = new Headers({
         'api-key': 'spark1234',
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -23,9 +28,16 @@ URLButton.addEventListener('click', async () => {
         body: `url=${url}`
     })
         .then(d => d.json())
-        .catch(console.error);
+        .catch(e => {
+            console.error(e);
+            loader.classList.add('invisible');
+        });
+    const sleep = () => new Promise(resolve => setTimeout(resolve, 2000));
+    // const res = {message: "LINK"};
+    await sleep();
+    loader.classList.add('invisible');
     // const result = document.createElement('div');
-    resultDiv.innerHTML = res.message;
+    resultDiv.innerHTML = `<a href="${res.message}" class="border-b border-teal-500 border-dashed">${res.message}</a>`;
     console.log('RES', res);
 });
 
