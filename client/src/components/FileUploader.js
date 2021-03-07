f2dbc1da3738fcc4a690bba553c788fb51371685
Sparkenstein <zetabytes.pp@gmail.com>
2021-03-07 19:50:51 +0530

    feat: passwod feature for file uploads

--- client/src/components/FileUploader.js
+++ client/src/components/FileUploader.js
@@ -2,12 +2,38 @@ import { useState } from 'react';
 
 function FileUploader() {
     const [showToast, setShowToast] = useState(false);
+    const [password, setPassword] = useState('');
     const [sucessMessage, setSuccessMessage] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [file, setFile] = useState('');
+    const [randomKey, setRandomkey] = useState(Math.random().toString(20));
+
+    const handleOnPasswordChanged = e => {
+        setPassword(e.target.value);
+    };
+
+    const handleOnPasswordToggle = () => {
+        setShowPassword(!showPassword);
+    };
     const handleUpload = async e => {
         e.preventDefault();
-        const files = e.target.files[0];
+        setFile(e.target.files[0]);
+        // const files = e.target.files[0];
+    };
+    const handleSubmit = async e => {
         const formData = new FormData();
-        formData.append('file', files);
+        console.log('File', file);
+        if (!file) {
+            console.log('No file selected');
+            setShowToast(true);
+            setSuccessMessage('Please Select a file to upload');
+            return;
+        }
+        formData.append('file', file);
+        if (password) {
+            console.log('attaching password', password);
+            formData.append('pass', password);
+        }
         const res = await fetch('/', {
             method: 'POST',
             headers: {
@@ -18,6 +44,8 @@ function FileUploader() {
         })
             .then(data => {
                 setShowToast(true);
+                setRandomkey(Math.random().toString(20));
+                setFile(null);
                 return data.text();
             })
             .catch(error => {
@@ -47,6 +75,7 @@ function FileUploader() {
                                     <input
                                         className="form-input"
                                         onChange={handleUpload}
+                                        key={randomKey}
                                         type="file"
                                         id="fileupload"
                                         placeholder="Name"
@@ -55,30 +84,79 @@ function FileUploader() {
                             </div>
                             <div className="card-footer">
                                 <div className="columns">
-                                    <div className="column col-3">
+                                    <div className="column col-5">
                                         <ul className="menu">
                                             <li
                                                 className="divider"
                                                 data-content="Options"
                                             ></li>
                                             <li className="menu-item">
+                                                <div className="">
+                                                    <div className="columns">
+                                                        <div className="column">
+                                                            <label className="form-checkbox">
+                                                                <input
+                                                                    type="checkbox"
+                                                                    onChange={
+                                                                        handleOnPasswordToggle
+                                                                    }
+                                                                />
+                                                                <i className="form-icon"></i>
+                                                                password
+                                                            </label>
+                                                        </div>
+                                                        {showPassword ? (
+                                                            <div className="column">
+                                                                <input
+                                                                    type="text"
+                                                                    className="form-input"
+                                                                    placeholder="enter password"
+                                                                    onChange={
+                                                                        handleOnPasswordChanged
+                                                                    }
+                                                                    value={
+                                                                        password
+                                                                    }
+                                                                ></input>
+                                                            </div>
+                                                        ) : null}
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li className="menu-item">
                                                 <label className="form-checkbox">
                                                     <input type="checkbox" />
                                                     <i className="form-icon"></i>
-                                                    form-checkbox
+                                                    expires
                                                 </label>
                                             </li>
                                         </ul>
                                     </div>
                                 </div>
+
+                                <button
+                                    style={{ marginTop: '15px' }}
+                                    className="btn btn-success"
+                                    onClick={handleSubmit}
+                                >
+                                    Submit
+                                </button>
                                 {showToast ? (
                                     <div
-                                        className="toast toast-success"
-                                        style={{ marginTop: '20px' }}
+                                        style={{ marginTop: '10px' }}
+                                        className="toast toast-primary"
                                     >
-                                        <a href={sucessMessage} target="_new">
-                                            {sucessMessage}
-                                        </a>
+                                        {sucessMessage !==
+                                        'Please Select a file to upload' ? (
+                                            <a
+                                                href={sucessMessage}
+                                                target="_new"
+                                            >
+                                                {sucessMessage}
+                                            </a>
+                                        ) : (
+                                            <div>{sucessMessage}</div>
+                                        )}
                                     </div>
                                 ) : null}
                             </div>
